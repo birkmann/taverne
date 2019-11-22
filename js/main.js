9ec29fd2ed2ecdd21eec1e3f7cfea2d902384500
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-11-22 14:02:13 +0100

    files

--- js/main.js
+++ js/main.js
@@ -30,42 +30,74 @@ var duration = 50; // duration in seconds
 var fadeAmount = 0.3; // fade duration amount relative to the time the image is visible
 
 $(document).ready(function (){
-  var images = $("#slideshow .item");
-  var numImages = images.size();
-  var durationMs = duration * 1000;
-  var imageTime = durationMs / numImages; // time the image is visible 
-  var fadeTime = imageTime * fadeAmount; // time for cross fading
-  var visibleTime = imageTime  - (imageTime * fadeAmount * 2);// time the image is visible with opacity == 1
-  var animDelay = visibleTime * (numImages - 1) + fadeTime * (numImages - 2); // animation delay/offset for a single image 
-  
-  images.each( function( index, element ){
-    if(index != 0){
-      $(element).css("opacity","0");
-      setTimeout(function(){
-        doAnimationLoop(element,fadeTime, visibleTime, fadeTime, animDelay);
-      },visibleTime*index + fadeTime*(index-1));
-    }else{
-      setTimeout(function(){
-        $(element).animate({opacity:0},fadeTime, function(){
-          setTimeout(function(){
-            doAnimationLoop(element,fadeTime, visibleTime, fadeTime, animDelay);
-          },animDelay )
-        });
-      },visibleTime);
-    }
-  });
+	var images = $("#slideshow .item");
+	var numImages = images.size();
+	var durationMs = duration * 1000;
+	var imageTime = durationMs / numImages; // time the image is visible 
+	var fadeTime = imageTime * fadeAmount; // time for cross fading
+	var visibleTime = imageTime  - (imageTime * fadeAmount * 2);// time the image is visible with opacity == 1
+	var animDelay = visibleTime * (numImages - 1) + fadeTime * (numImages - 2); // animation delay/offset for a single image 
+
+	images.each( function( index, element ){
+		if(index != 0){
+			$(element).css("opacity","0");
+				setTimeout(function(){
+					doAnimationLoop(element,fadeTime, visibleTime, fadeTime, animDelay);
+				},visibleTime*index + fadeTime*(index-1));
+		} else{
+			setTimeout(function(){
+				$(element).animate({opacity:0},fadeTime, function(){
+					setTimeout(function(){
+						doAnimationLoop(element,fadeTime, visibleTime, fadeTime, animDelay);
+					}, animDelay )
+				});
+			},visibleTime);
+		}
+	});
 });
 
 // creates a animation loop
 function doAnimationLoop(element, fadeInTime, visibleTime, fadeOutTime, pauseTime){
-  fadeInOut(element,fadeInTime, visibleTime, fadeOutTime ,function(){
-    setTimeout(function(){
-      doAnimationLoop(element, fadeInTime, visibleTime, fadeOutTime, pauseTime);
-    },pauseTime);
-  });
+	fadeInOut(element,fadeInTime, visibleTime, fadeOutTime ,function(){
+		setTimeout(function(){
+			doAnimationLoop(element, fadeInTime, visibleTime, fadeOutTime, pauseTime);
+		},pauseTime);
+	});
 }
 
 // shorthand for in- and out-fading
 function fadeInOut( element, fadeIn, visible, fadeOut, onComplete){
-  return $(element).animate( {opacity:1}, fadeIn ).delay( visible ).animate( {opacity:0}, fadeOut, onComplete);
+	return $(element).animate( {opacity:1}, fadeIn ).delay( visible ).animate( {opacity:0}, fadeOut, onComplete);
+}
+
+var cover = document.querySelector('.js-parallax'),
+coverHeight = Math.round(cover.offsetHeight),
+translate,
+parallaxThreshold = 3;
+
+function parallax() {
+	if (window.scrollY < coverHeight) {
+		translate = Math.round(window.scrollY / parallaxThreshold);
+		window.requestAnimationFrame(function () {
+			cover.style.transform = 'translateY(' + translate + 'px)';
+		});
+	}
+}
+
+parallax();
+
+window.addEventListener('scroll', parallax, false);
+
+window.addEventListener('resize', debounce(function () {
+	coverHeight = Math.round(cover.offsetHeight);
+}, 500));
+
+function debounce(fn, wait) {
+	var timeout;
+	return function () {
+		clearTimeout(timeout);
+		timeout = setTimeout(function () {
+			fn.apply(this, arguments)
+		}, (wait || 1));
+	}
 }
